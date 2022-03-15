927899b0c1ac0a9b04326b00e80db1fcf4365d19
seecashew <101618564+seecashew@users.noreply.github.com>
2022-03-15 00:55:10 -0400

    Add files via upload

---
+++ demoapp.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 15 00:54:19 2022
+
+@author: andu
+"""
+
+import streamlit as st
+import plotly as py
+import plotly.io as pio
+from plotly import graph_objects as go
+
+
+
+
+labels =  ["People who like streamlit", "People who don't know about streamlit"]
+values = [80, 20]
+
+
+# pull is given as a fraction of the pie radius
+fig = go.Figure(data=[go.Pie(labels=labels, values=values,textinfo='label+percent',
+                             insidetextorientation='radial', pull=[0, 0, 0.2, 0] #,hole = 0.2
+                            )])
+
+
+fig.update_layout(
+    title_text="Demo streamlit app")
+
+
+st.plotly_chart(fig, sharing='streamlit')
